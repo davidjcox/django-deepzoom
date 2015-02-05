8d3ed0d6236fb3a2b4a2bb00b345d9e12c17c96f
David J Cox <davidjcox.at@gmail.com>
2015-02-05 14:05:54 -0600

    version 3.0 testmodels addition

---
+++ deepzoom/test/models.py
@@ -0,0 +1,14 @@
+'''django-deepzoom test models'''
+
+from deepzoom.models import UploadedImage
+
+
+
+class TestImage(UploadedImage):
+    '''
+    Test class.
+    '''
+# /TestImage
+
+
+#EOF - django-deepzoom test models
